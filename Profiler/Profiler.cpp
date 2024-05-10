6c8c6a497cda0328688d15dbcfaa984408cfcf13
Jeroen van den Heuvel <jeroenvandenheuvel@live.nl>
2024-05-10 20:12:36 +0200

    use __counter__ for profiler

--- Profiler/Profiler.cpp
+++ Profiler/Profiler.cpp
@@ -1,5 +1,7 @@
-#define TimeFunction() auto _ = Profiler::_timeNamed(__FUNCTION__)
-#define TimeBlock(name) auto _ = Profiler::_timeNamed(name)
+#define NameConcat2(A, B) A##B
+#define NameConcat(A, B) NameConcat2(A, B)
+#define TimeFunction() TimeBlock(__FUNCTION__)
+#define TimeBlock(name) auto NameConcat(Block, __LINE__) = Profiler::_timeNamed(name, __COUNTER__ +1) // global counter only works with 1 translation unit
 #define EndAndPrintResults() Profiler::_endAndPrintResults()
 #define StartProfiling() Profiler()
 #include "../ApproximateCPUTimerFreq/ApproximateCPUTimerFreq.cpp"
@@ -27,7 +29,7 @@ private:
 	static const int maxCount = 1000;
 	static u64 _cpuTimeFreq;
 	static u64 _cpuTimeStart;
-	static Timing _timings[maxCount];
+	static AggregateTiming _timings[maxCount];
 	static int _timingsCount;
 	public:
 	Profiler() {
@@ -40,66 +42,45 @@ private:
 		u64 cpuTimeElapsed = cpuTimeEnd - _cpuTimeStart;
 		double cpuTimeElapsedMs = (double)cpuTimeElapsed * 1000 / (double)_cpuTimeFreq;
 		cout << "CPU Time Elapsed: " << cpuTimeElapsedMs << " ms" << std::endl;
-		AggregateTiming aggTimings[maxCount];
-		int i = 0;
-		int j = 0;
-		int aggTimingsCount = 0;
-		//aggregate
-		for (int i = 0; i < _timingsCount; ++i) {
-			Timing timing = _timings[i];
-			bool found = false;
-			for (int j = 0; j < aggTimingsCount; ++j) {
-				if (aggTimings[j].name == timing.name) {
-					aggTimings[j].time += timing.end - timing.start;
-					found = true;
-					break;
-				}
-			}
-			if (!found) {
-				if (aggTimingsCount >= maxCount) {
-					cout << "Error: too many timings" << std::endl;
-				}
-				aggTimings[aggTimingsCount].name = timing.name;
-				aggTimings[aggTimingsCount].time = timing.end - timing.start;
-				++aggTimingsCount;
-			}
-		}
-		//print
-		for (int i = 0; i < aggTimingsCount; ++i) {
-			auto time = double(aggTimings[i].time) * 1000 / _cpuTimeFreq;
-			aggTimings[i].percentage = time * 100 / cpuTimeElapsedMs;
-			cout << "Timing: " << aggTimings[i].name << " Time: " << time << "ms" << "(" << aggTimings[i].percentage << "%)" << std::endl;
+
+		for (int i = 1; i <= _timingsCount; ++i) {
+			auto time = double(_timings[i].time) * 1000 / _cpuTimeFreq;
+			_timings[i].percentage = time * 100 / cpuTimeElapsedMs;
+			cout << "Timing: " << _timings[i].name << " Time: " << time << "ms" << "(" << _timings[i].percentage << "%)" << std::endl;
 		}
 	}
-	static void SumbitTiming(string name, u64 start, u64 end) {
-		if (_timingsCount >= maxCount) {
+	static void SumbitTiming(string name, int index, u64 start, u64 end) {
+		if (index >= maxCount) {
 			cout << "Error: too many timings" << std::endl;
 		}
 		Timing timing;
 		timing.name = name;
 		timing.start = start;
 		timing.end = end;
-		_timings[_timingsCount] = timing;
-		++_timingsCount;
+		_timings[index].name = name;
+		_timings[index].time += end - start;
+		_timingsCount = max(_timingsCount, index);
 	}
 
 	class Timer
 	{
 		string _name;
 		u64 _start;
+		int _index;
 	public:
-		Timer(string name) {
+		Timer(string name, int index) {
 			_name = name;
 			_start = __rdtsc();
+			_index = index;
 		}
 		~Timer() {
 			auto end = __rdtsc();
-			Profiler::SumbitTiming(_name, _start, end);
+			Profiler::SumbitTiming(_name, _index, _start, end);
 		}
 	};
 	
-	static Timer _timeNamed(const string& name) {
-		return Timer(name);
+	static Timer _timeNamed(const string& name, int index) {
+		return Timer(name, index);
 	}
 	static void Reset();
 
@@ -108,4 +89,4 @@ private:
 u64 Profiler::_cpuTimeFreq;
 u64 Profiler::_cpuTimeStart;
 int Profiler::_timingsCount;
-Timing Profiler::_timings[maxCount];
+AggregateTiming Profiler::_timings[maxCount];
