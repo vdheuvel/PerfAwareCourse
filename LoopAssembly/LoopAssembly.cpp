ac3d8aa430a93a8fe567bb87be072c5d60fd3c46
Jeroen van den Heuvel <jvandenheuvel@expertdoc.nl>
2026-01-04 16:43:25 +0100

    3.5 inspecting loop assembly

---
+++ LoopAssembly/LoopAssembly.cpp
@@ -0,0 +1,56 @@
+#define PROFILER 1
+#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <Windows.h>
+#include <intrin.h>
+#include "../Profiler/ApproximateCPUTimerFreq.cpp"
+#include <iomanip>
+#include "../Profiler/Profiler.cpp"
+#include <psapi.h>
+#include "../Profiler/RepetitionTester.cpp"
+
+using f64 = double;
+using u8 = short;
+using std::string;
+using std::vector;
+using std::cout;
+using hrclock = std::chrono::high_resolution_clock;
+using namespace std::chrono_literals;
+using u32 = unsigned int;
+
+u64 cpuTimeFreq;
+int pageCount = 1024;
+int pageSize = 4096;
+
+// to convince compiler that const size is not const, so it uses 3 byte compare (same as casey's)
+// otherwise it'll use 7 byte compare
+u64 opaque(u64 x) { return x; } 
+
+void Test(u64 size) {
+    RepetitionTester tester(10, pageSize * pageCount, cpuTimeFreq);
+    tester.Start();
+    while (!tester.IsDone()) {
+        char* buffer = (char*)VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+        for (u64 i = 0; i < size; ++i) {
+            buffer[i] = (u8)i;
+        }
+        tester.SubmitRepetition();
+    }
+    tester.Finish();
+    tester.Print();
+}
+
+
+int main()
+{
+    cpuTimeFreq = getCpuTimerFreq(1000000);
+    cout << "cpu time freq " << cpuTimeFreq << std::endl;
+
+    u64 size = pageCount * pageSize;
+    Test(opaque(size));
+}
